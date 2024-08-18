806061032e1bac1ee37946b4f36b50f45a80ae5e
Matias P. Menendez <121203087+Vontk@users.noreply.github.com>
2024-08-18 19:45:02 -0300

    Update Sets.java

--- src/main/java/com/collections/Sets.java
+++ src/main/java/com/collections/Sets.java
@@ -1,13 +1,29 @@
 package com.collections;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.List;
 import java.util.Arrays;
 
 public class Sets {
+    public static void main(String[] args) {
+        //verificando que efetivamente el codigo proporcionado cree una variable del tipo set
+        System.out.println(ALCOHOLS);
+    }
+    private static final Set<String> ALCOHOLS = new HashSet<>(Arrays.asList("whiskey", "whisky", "white rum",
+            "dark rum", "bourbon", "rye", "scotch", "vodka", "tequila", "gin", "dry vermouth", "sweet vermouth",
+            "prosecco","aperol", "brandy", "mezcal", "triple sec", "coffee liqueur", "almond liqueur", "champagne",
+            "orange curacao", "rum"));
 
-    private static final Set<String> ALCOHOLS = new HashSet<>(Arrays.asList("whiskey", "whisky", "white rum", "dark rum", "bourbon", "rye", "scotch", "vodka",
-            "tequila", "gin", "dry vermouth", "sweet vermouth", "prosecco","aperol", "brandy", "mezcal",
-            "triple sec", "coffee liqueur", "almond liqueur", "champagne", "orange curacao", "rum"));
-
+    String check_drink(String item, ArrayList<String> list) {
+        String reformat;
+        for (int i = 0; i < list.size(); i++) {
+            if (ALCOHOLS.contains(list.get(i))) {
+                reformat = "%s Cocktail";
+                return String.format(reformat, item);
+            }
+        }
+        reformat = "%s Mocktail";
+        return String.format(reformat, item);
+    }
 }
