134d7ecbdf14c58753766860cfcaf9872f454946
Matias P. Menendez <121203087+Vontk@users.noreply.github.com>
2024-08-18 19:43:34 -0300

    Update Lists.java

--- src/main/java/com/collections/Lists.java
+++ src/main/java/com/collections/Lists.java
@@ -1,7 +1,49 @@
-package com.collections;
+package collections;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class Lists {
-
+    public static int index_of(String search_item, ArrayList<String> list) {
+        int index = -1;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(search_item)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static int index_of_by_index(String search_item, ArrayList<String> list, int search_index) {
+        int index = -1;
+        for (int i = search_index; i < list.size(); i++) {
+            if (list.get(i).equals(search_item)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    int index_of_empty(ArrayList<String> list) {
+        int index = -1;
+        for (int i = 0; i < list.size(); i++) {
+            // aca habia puesto .equals("") pero el ide me lo quiso remplazar por .isEmpty()
+            if (list.get(i).isEmpty()) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    int put(String item, ArrayList<String> list) {
+        int index = index_of_empty(list);
+        if (index != -1) {
+            list.set(index, item);
+        }
+        return index;
+    }
+    int remove(String item, ArrayList<String> list) {
+        int numberOfEliminations = 0;
+        while (index_of(item, list) != -1) {
+            list.set(index_of(item, list), "");
+            numberOfEliminations++;
+        }
+        return numberOfEliminations;
+    }
 }
