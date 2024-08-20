afd4815f5cf61229e90bcd80801fb688b4107bd6
Matias P. Menendez <121203087+Vontk@users.noreply.github.com>
2024-08-19 23:16:03 -0300

    Update Lists.java

--- src/main/java/com/collections/Lists.java
+++ src/main/java/com/collections/Lists.java
@@ -1,47 +1,43 @@
 package com.collections;
-
-import java.util.ArrayList;
+import java.util.List;
 
 public class Lists {
-    public static int index_of(String search_item, ArrayList<String> list) {
-        int index = -1;
+    public static int indexOf(String search_item, List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(search_item)) {
-                index = i;
+                return i;
             }
         }
-        return index;
+        return -1;
     }
-    public static int index_of_by_index(String search_item, ArrayList<String> list, int search_index) {
-        int index = -1;
+    public static int indexOfByIndex(String search_item, List<String> list, int search_index) {
         for (int i = search_index; i < list.size(); i++) {
             if (list.get(i).equals(search_item)) {
-                index = i;
+                return i;
             }
         }
-        return index;
+        return -1;
     }
-    int index_of_empty(ArrayList<String> list) {
-        int index = -1;
+    public static int indexOfEmpty(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             // aca habia puesto .equals("") pero el ide me lo quiso remplazar por .isEmpty()
             if (list.get(i).isEmpty()) {
-                index = i;
+                return i;
             }
         }
-        return index;
+        return -1;
     }
-    int put(String item, ArrayList<String> list) {
-        int index = index_of_empty(list);
+    public static int put(String item, List<String> list) {
+        int index = indexOfEmpty(list);
         if (index != -1) {
             list.set(index, item);
         }
         return index;
     }
-    int remove(String item, ArrayList<String> list) {
+    public static int remove(String item, List<String> list) {
         int numberOfEliminations = 0;
-        while (index_of(item, list) != -1) {
-            list.set(index_of(item, list), "");
+        while (indexOf(item, list) != -1) {
+            list.set(indexOf(item, list), "");
             numberOfEliminations++;
         }
         return numberOfEliminations;
